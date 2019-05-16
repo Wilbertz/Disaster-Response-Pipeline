d1abf28dfb42c7373ddbefac19bf623ba9f971e5
Harald Wilbertz <Harald Wilbertz@gmx.de>
2019-05-16 22:05:40 +0200

    test: Unit tests for classifier added

---
+++ tests/train_classifier_test.py
@@ -0,0 +1,76 @@
+"""train_classifier_test: Unit tests for training a classifier."""
+
+__author__ = "Harald Wilbertz"
+__version__ = "1.0.0"
+
+import os
+import sys
+import unittest
+
+sys.path.append('../models')
+from train_classifier import tokenize
+
+
+class TrainClassifierTestCase(unittest.TestCase):
+    """Tests for methods defined in train classifier.py"""
+
+    def test_load_data_should_convert_to_lowercase(self):
+        """Does the tokenize function convert to lowercase ?"""
+        # Arrange
+        text = "Please, we need tents and water. We are in Silo, Thank you!"
+
+        # Act
+        result = tokenize(text)
+
+        # Assert
+        self.assertIsNotNone(result)
+        self.assertFalse('Silo' in result)
+        self.assertTrue('silo' in result)
+
+    def test_load_data_should_remove_punctuation(self):
+        """Does the tokenize function remove punctuation ?"""
+        # Arrange
+        text = "Please, we need tents and water. We are in Silo, Thank you! My name is Dr. Who"
+
+        # Act
+        result = tokenize(text)
+        print(result)
+
+        # Assert
+        self.assertIsNotNone(result)
+
+    def test_load_data_should_lemmatize_text(self):
+        """Does the tokenize function lemmatize words ?"""
+        # Arrange
+        text = "Please, we need tents and water. We are in Silo, Thank you!"
+
+        # Act
+        result = tokenize(text)
+
+        # Assert
+        self.assertTrue('tent' in result)
+
+    def test_load_data_should_remove_stopwords(self):
+        """Does the tokenize function remove stop words ?"""
+        # Arrange
+        text = "Please, we need tents and water. We are in Silo, Thank you!"
+
+        # Act
+        result = tokenize(text)
+
+        # Assert
+        self.assertFalse('we' in result)
+        self.assertFalse('you' in result)
+        self.assertFalse('and' in result)
+        self.assertFalse('in' in result)
+
+
+def suite():
+    """Define the test suit used by the test runner."""
+    return unittest.defaultTestLoader.loadTestsFromTestCase(TrainClassifierTestCase)
+
+
+if __name__ == '__main__':
+    """Run the unit tests."""
+    runner = unittest.TextTestRunner()
+    runner.run(suite())

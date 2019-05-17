8aab91cd3347264b977277fd855041a38144f623
Harald Wilbertz <Harald Wilbertz@gmx.de>
2019-05-17 21:22:20 +0200

    doc: Docstrings added to methods

--- app/run.py
+++ app/run.py
@@ -6,33 +6,39 @@ __version__ = "1.0.0"
 import re
 import json
 from typing import List, Any, Union
-
 import pandas as pd
-
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-
 from flask import Flask
 from flask import render_template, request
-
 import plotly
 # import as pgo and not go in order to avoid name clash with go controller method
 import plotly.graph_objs as pgo
-
 from sklearn.externals import joblib
-
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
 
 
-def tokenize(text: str) -> List[Union[str, Any]]:
+def tokenize(text: str) -> List[str]:
+    """
+       Tokenize the given text, this includes the following steps:
+       - punctuation removal
+       - tokenize words
+       - stopwords removal
+       - lemmatize words
+
+       arguments:
+           text (string): input text to be tokenized.
+       returns:
+           tokens (list of strings) : list of tokens
+    """
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower()).strip()
 
     # tokenize text
-    tokens: List[Union[str, Any]] = word_tokenize(text)
+    tokens = word_tokenize(text)
 
     # lemmatize and remove stop words
     lemmatizer = WordNetLemmatizer()
@@ -45,7 +51,7 @@ def tokenize(text: str) -> List[Union[str, Any]]:
 engine = create_engine('sqlite:///../data/DisasterResponse.sqlite')
 df = pd.read_sql_table('Messages', engine)
 
-# load model
+# load model from pickl file
 model = joblib.load("../models/classifier.pkl")
 
 
@@ -55,17 +61,14 @@ model = joblib.load("../models/classifier.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
-    df_top_10_cat = df.iloc[:, 4:].sum(axis=0).sort_values(ascending=False)[:10]
-    top_10_cat_names = list(df_top_10_cat.index.values)
-    top_10_cat_values = list(df_top_10_cat.values)
+    top_categories = df.iloc[:, 4:].sum(axis=0).sort_values(ascending=False)[:10]
+    top_category_names = list(top_categories.index.values)
+    top_category_values = list(top_categories.values)
 
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
@@ -84,28 +87,27 @@ def index():
                     'title': "Genre"
                 }
             }
-        }
-    ]
-    graph_2 = {
-        'data': [
-            pgo.Bar(
-                x=top_10_cat_names,
-                y=top_10_cat_values
-            )
-        ],
+        },
+        {
+            'data': [
+                pgo.Bar(
+                    x=top_category_names,
+                    y=top_category_values
+                )
+            ],
 
-        'layout': {
-            'title': 'Top 10 categories',
-            'yaxis': {
-                'title': "Count"
-            },
-            'xaxis': {
-                'title': "Categories"
+            'layout': {
+                'title': 'Top 10 categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
             }
         }
-    }
-    graphs.append(graph_2)
-    
+    ]
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graph_json = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
